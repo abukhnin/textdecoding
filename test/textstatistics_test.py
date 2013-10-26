8c6180ebe2aebed602315a46fa8919a28c064c13
Alexey Bukhnin <alexey.bukhnin@gmail.com>
2013-10-26 13:18:02 +0400

    Add functions to split text to words and ger word frequencies

--- test/textstatistics_test.py
+++ test/textstatistics_test.py
@@ -12,8 +12,11 @@ sys.path.append(root_path)
 
 import textstatistics
 
+QUOTE_FROM_TOLSTOY = \
+        u'Не слушайте тех, кто говорит дурно о других и хорошо о вас.'
+QUOTE_FROM_SHAKESPEARE= "To be, or not to be?"
 
-class Test(unittest.TestCase):
+class TestGetCharFrequencies(unittest.TestCase):
     def test_get_char_frequencies_empty(self):
         text = u''
         result = textstatistics.get_char_frequencies(text)
@@ -27,7 +30,7 @@ class Test(unittest.TestCase):
         self.assertDictEqual(result, expected)
 
     def test_get_char_frequencies_simple_text(self):
-        text = u'Не слушайте тех, кто говорит дурно о других и хорошо о вас.'
+        text = QUOTE_FROM_TOLSTOY
         result = textstatistics.get_char_frequencies(text)
         expected = {
                     u'Н': 1,
@@ -52,6 +55,63 @@ class Test(unittest.TestCase):
                     u'н': 1,
                     u'.': 1,
                     }
+        self.assertDictEqual(result, expected)
+
+
+class TestWordFunctions(unittest.TestCase):
+    def test_split_to_words_english(self):
+        text = QUOTE_FROM_SHAKESPEARE
+        expected = ['To', 'be', 'or', 'not', 'to', 'be']
+        result = textstatistics.split_to_words(text)
+        self.assertEqual(result, expected)
+
+    def test_split_to_words_russian(self):
+        text = QUOTE_FROM_TOLSTOY
+        expected = [
+                    u'Не',
+                    u'слушайте',
+                    u'тех',
+                    u'кто',
+                    u'говорит',
+                    u'дурно',
+                    u'о',
+                    u'других',
+                    u'и',
+                    u'хорошо',
+                    u'о',
+                    u'вас'
+                    ]
+        result = textstatistics.split_to_words(text)
+        self.assertEqual(result, expected)
+
+    def test_get_word_frequencies_empty(self):
+        text = ''
+        expected = {}
+        result = textstatistics.get_word_frequencies(text)
+        self.assertEqual(result, expected)
+
+    def test_get_word_frequencies_english(self):
+        text = QUOTE_FROM_SHAKESPEARE
+        expected = {'To': 1, 'be': 2, 'or': 1, 'not': 1, 'to': 1}
+        result = textstatistics.get_word_frequencies(text)
+        self.assertDictEqual(result, expected)
+
+    def test_get_word_frequencies_russian(self):
+        text = QUOTE_FROM_TOLSTOY
+        expected = {
+                    u'Не': 1,
+                    u'слушайте': 1,
+                    u'тех': 1,
+                    u'кто': 1,
+                    u'говорит': 1,
+                    u'дурно': 1,
+                    u'о': 2,
+                    u'других': 1,
+                    u'и': 1,
+                    u'хорошо': 1,
+                    u'вас': 1
+                    }
+        result = textstatistics.get_word_frequencies(text)
         self.assertDictEqual(result, expected)
 
 
